8e3ac4f06db16bdb67b7f9b0ec12c2999b279190
WAF-Eisbaer <info@flopunkt.de>
2014-06-01 16:36:22 +0200

    Update 2014-06-01/3
    Fix

--- public/index.php
+++ public/index.php
@@ -38,15 +38,15 @@ include("./php/config.php");
 			<div class="navbar-header">
 				<a class="navbar-brand" href="index.php"><?print $title;?></a>
 				<ul class="nav navbar-nav navbar-right">
-					<li class="dropdown">
+					<!--<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> AltisLife Public Server <b class="caret"></b></a>
-						<!--<ul class="dropdown-menu">
+						<ul class="dropdown-menu">
 							<li><a href="">AltisLife Public Server</a></li>
 							<?php if($admin->whitelist == 1){ ?>
 								<li><a href="../wlwhitelist">AltisLife Whitelist Server</a></li>
 							<?php } ?>
-						</ul>-->
-					</li>
+						</ul>
+					</li>-->
 				</ul>
 			</div>
 		</div>
